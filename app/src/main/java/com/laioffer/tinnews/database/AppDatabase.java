9ba4c9f14be6fda3759e8257422eed6e2cb77c97
enochxoo <yl3744@columbia.edu>
2019-04-10 16:47:43 -0400

    detail

--- app/src/main/java/com/laioffer/tinnews/database/AppDatabase.java
+++ app/src/main/java/com/laioffer/tinnews/database/AppDatabase.java
@@ -5,6 +5,7 @@ import android.arch.persistence.room.RoomDatabase;
 
 import com.laioffer.tinnews.retrofit.response.News;
 
+//7.5
 @Database(entities = {News.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract NewsDao newsDao();
